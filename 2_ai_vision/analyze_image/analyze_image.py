8bf909574a0c56b77b38ed8d202505be0828e276
Hiroki Nomura <nomhiro1204@gmail.com>
2024-10-12 16:31:48 +0900

    Add devcontainer configuration for Python 3 and analyze_image module
    - Added .devcontainer/devcontainer.json file for configuring the Python 3 development container.
    - Created analyze_image module with initial code for image analysis using Azure Cognitive Services.
    - Added requirements.txt file with necessary dependencies for the analyze_image module.
    - Ignored .env file in the analyze_image directory.

--- 2_ai_vision/analyze_image/analyze_image.py
+++ 2_ai_vision/analyze_image/analyze_image.py
@@ -1,1 +1,28 @@
-print("hello world")
+import os
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
+
+# Set the values of your computer vision endpoint and computer vision key
+# as environment variables:
+try:
+    endpoint = os.environ["VISION_ENDPOINT"]
+    key = os.environ["VISION_KEY"]
+except KeyError:
+    print("Missing environment variable 'VISION_ENDPOINT' or 'VISION_KEY'")
+    print("Set them before running this sample.")
+    exit()
+
+# Create an Image Analysis client for synchronous operations,
+# using API key authentication
+client = ImageAnalysisClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(key)
+)
+
+result = client.analyze(
+    image_url="<url>",
+    visual_features=[VisualFeatures.CAPTION, VisualFeatures.READ],
+    gender_neutral_caption=True,
+    language="en",
+)
