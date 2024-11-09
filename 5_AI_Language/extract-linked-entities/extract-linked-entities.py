542e22adb442d2bc3897730c198051a47fa03f8c
Hiroki Nomura <nomhiro1204@gmail.com>
2024-11-09 21:01:31 +0000

    Refactor AI Language code for extracting linked entities

--- 5_AI_Language/extract-linked-entities/extract-linked-entities.py
+++ 5_AI_Language/extract-linked-entities/extract-linked-entities.py
@@ -4,34 +4,48 @@ from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
 # 環境変数からキーとエンドポイントを取得
-language_key = os.environ.get('LANGUAGE_KEY')
-language_endpoint = os.environ.get('LANGUAGE_ENDPOINT')
+language_endpoint = os.getenv("LANGUAGE_ENDPOINT")
+language_key = os.getenv("LANGUAGE_KEY")
 
-# テキスト分析クライアントの作成
 ta_credential = AzureKeyCredential(language_key)
-client = TextAnalyticsClient(
-        endpoint=language_endpoint, 
-        credential=ta_credential)
+ta_client = TextAnalyticsClient(
+  endpoint=language_endpoint,
+  credential=ta_credential
+)
 
 try:
-    # ./docs.mdファイルを読み込み、documentsリストに格納
-    with open('./docs.md', 'r') as f:
-        documents = [f.read()]
-    
-    # テキスト分析APIを使用してキーフレーズを抽出
-    result = client.recognize_linked_entities(documents = documents)[0]
-    
-    print("Linked Entities:\n")
-    for entity in result.entities:
-        print("\tName: ", entity.name, "\tId: ", entity.data_source_entity_id, "\tUrl: ", entity.url,
-        "\n\tData Source: ", entity.data_source)
-        print("\tMatches:")
-        for match in entity.matches:
-            print("\t\tText:", match.text)
-            print("\t\tConfidence Score: {0:.2f}".format(match.confidence_score))
-            print("\t\tOffset: {}".format(match.offset))
-            print("\t\tLength: {}".format(match.length))
-
+  # ./docs.mdファイルを読み込み、documentsリストに格納
+  with open('./docs.md', 'r') as f:
+    documents = [f.read()]
+  
+  # テキスト分析APIを使用してキーフレーズを抽出
+  result = ta_client.recognize_linked_entities(documents = documents)[0]
+  
+  # Json形式で出力するためのリストを作成
+  output = []
+  # エンティティごとに情報を取得し、outputリストに格納
+  for entity in result.entities:
+    entity_info = {
+      "Name": entity.name,
+      "Id": entity.data_source_entity_id,
+      "Url": entity.url,
+      "Data Source": entity.data_source,
+      "Matches": []
+    }
+    # エンティティにマッチした単語情報を取得
+    for match in entity.matches:
+      match_info = {
+        "Text": match.text,
+        "Confidence Score": round(match.confidence_score, 2),
+        "Offset": match.offset,
+        "Length": match.length
+      }
+      entity_info["Matches"].append(match_info)
+    output.append(entity_info)
+  
+  # result.jsonに結果を書き込み
+  with open('./result.json', 'w') as f:
+    json.dump(output, f, ensure_ascii=False, indent=2)
 
 except Exception as err:
-    print("Encountered exception. {}".format(err))
+  print("Encountered exception. {}".format(err))
