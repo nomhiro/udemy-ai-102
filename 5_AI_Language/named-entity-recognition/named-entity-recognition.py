01f592efae98bf0bb90801b510624ab95bd67535
Hiroki Nomura <nomhiro1204@gmail.com>
2024-11-10 05:46:37 +0000

    Refactor named-entity-recognition code for AI Language and add entity extraction result output

--- 5_AI_Language/named-entity-recognition/named-entity-recognition.py
+++ 5_AI_Language/named-entity-recognition/named-entity-recognition.py
@@ -1,36 +1,41 @@
 import os
+import json
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
 # 環境変数からキーとエンドポイントを取得
-language_key = os.environ.get('LANGUAGE_KEY')
-language_endpoint = os.environ.get('LANGUAGE_ENDPOINT')
+language_endpoint = os.getenv("LANGUAGE_ENDPOINT")
+language_key = os.getenv("LANGUAGE_KEY")
 
-# テキスト分析クライアントの作成
 ta_credential = AzureKeyCredential(language_key)
-client = TextAnalyticsClient(
-        endpoint=language_endpoint, 
-        credential=ta_credential)
+ta_client = TextAnalyticsClient(
+  endpoint=language_endpoint,
+  credential=ta_credential
+)
 
 try:
-    # ./docs.mdファイルを読み込み、documentsリストに格納
-    with open('./docs.md', 'r') as f:
-        documents = [f.read()]
+  # docs.mdファイルを読み込み、documentsリストに格納
+  with open("docs.md", "r") as f:
+    documents = [f.read()]
+  
+  # テキスト分析APIを使用して名前付きエンティティを抽出
+  result = ta_client.recognize_entities(documents=documents)[0]
+  
+  entities_list = []
+  for entity in result.entities:
+    entity_info = {
+      "Text": entity.text,
+      "Category": entity.category,
+      "SubCategory": entity.subcategory,
+      "Confidence Score": round(entity.confidence_score, 2),
+      "Length": entity.length,
+      "Offset": entity.offset
+    }
+    entities_list.append(entity_info)
+  
+  # 結果をJSON形式でresult.jsonに出力
+  with open("./result.json", "w") as f:
+    f.write(json.dumps(entities_list, ensure_ascii=False, indent=2))
     
-    # テキスト分析APIを使用してキーフレーズを抽出
-    result = client.recognize_linked_entities(documents = documents)[0]
-    
-    print("Linked Entities:\n")
-    for entity in result.entities:
-        print("\tName: ", entity.name, "\tId: ", entity.data_source_entity_id, "\tUrl: ", entity.url,
-        "\n\tData Source: ", entity.data_source)
-        print("\tMatches:")
-        for match in entity.matches:
-            print("\t\tText:", match.text)
-            print("\t\tConfidence Score: {0:.2f}".format(match.confidence_score))
-            print("\t\tOffset: {}".format(match.offset))
-            print("\t\tLength: {}".format(match.length))
-
-
 except Exception as err:
-    print("Encountered exception. {}".format(err))
+  print("Encountered exception. {}".format(err))
