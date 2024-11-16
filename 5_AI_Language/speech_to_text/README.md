e5546dd98b69d75bdda436d296e0748e1450d06a
Hiroki Nomura <nomhiro1204@gmail.com>
2024-11-16 19:46:18 +0900

    Modify Speech To Text Sample

--- 5_AI_Language/speech_to_text/README.md
+++ 5_AI_Language/speech_to_text/README.md
@@ -1,7 +1,17 @@
-必要なオーディオライブラリがインストールされているか確認します。以下のコマンドをターミナルで実行して、必要なライブラリをインストールします。
+必要なオーディオライブラリがOSにインストールされている必要があります。
+
+### Linux環境の場合
+以下のコマンドをターミナルで実行して、必要なライブラリをインストールします。
 
 ```bash
 sudo apt-get update
-sudo apt-get install alsa-utils
-sudo apt-get install libasound2
+sudo apt-get install ffmpeg
 ```
+
+### WindowsOSの場合
+ffmpegをインストールする手順は以下の通りです。
+- FFmpegの公式サイトからWindows用のビルドをダウンロードします。
+  https://ffmpeg.org/download.html
+- ダウンロードしたZIPファイルを解凍します。
+- 解凍したフォルダを任意の場所に移動します（例: C:\ffmpeg）。
+- 環境変数にffmpegのパスを追加します。
--- 5_AI_Language/speech_to_text/speech_to_text.py
+++ 5_AI_Language/speech_to_text/speech_to_text.py
@@ -1,6 +1,6 @@
 import os
-import azure.cognitiveservices.speech as speechsdk
 from pydub import AudioSegment
+import azure.cognitiveservices.speech as speechsdk
 
 # Convert m4a to wav
 audio = AudioSegment.from_file("audio_sample.m4a", format="m4a")
@@ -9,26 +9,23 @@ audio.export("./audio_sample.wav", format="wav")
 SPEECH_KEY = os.environ.get('SPEECH_KEY')
 SPEECH_REGION = os.environ.get('SPEECH_REGION')
 
-def recognize_from_microphone():
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+try:
+    # SpeechConfigを作成
     speech_config = speechsdk.SpeechConfig(subscription=SPEECH_KEY, region=SPEECH_REGION)
     speech_config.speech_recognition_language="ja-JP"
 
+    # AudioConfigを作成　wavファイルを指定
     audio_config = speechsdk.audio.AudioConfig(filename="audio_sample.wav")
+    # SpeechRecognizerを作成
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-    print("Speak into your microphone.")
+    # 音声認識を実行
     speech_recognition_result = speech_recognizer.recognize_once_async().get()
 
+    # 結果を表示
     if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
         print("Recognized: {}".format(speech_recognition_result.text))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = speech_recognition_result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-            print("Did you set the speech resource key and region values?")
 
-recognize_from_microphone()
+except Exception as err:
+    print("Encountered exception. {}".format(err))
+    
